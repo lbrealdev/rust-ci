7cf78bfe15df8fc85a88b0eae3416edc362ff75b
lbrealdev <lbrealdeveloper@gmail.com>
2022-04-21 01:56:49 +0200

    feat: Add new pub function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+pub fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -14,5 +18,15 @@ mod test {
     #[test]
     fn test_add_with_negative() {
         assert_eq!(add(-1, -2), -3);
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(sub(6, 3), 3);
+    }
+
+    #[test]
+    fn test_sub_with_negative() {
+        assert_eq!(sub(-15, -7), -8)
     }
 }
